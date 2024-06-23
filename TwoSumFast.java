654342b6f8c24c308d9377d3ed30b221861a74f2
Yadong Zhang <zhyd007@gmail.com>
2024-06-23 08:00:15 +0000

    Union Find

---
+++ TwoSumFast.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+public class TwoSumFast {
+    public static int count(int[] a) {
+        int n = a.length;
+        Arrays.sort(a);
+        if (containsDuplicates(a)) throw new IllegalArgumentException("array contains duplicate integers");
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            int j = Arrays.binarySearch(a, -a[i]);
+            if (j > i) count++;
+        }
+        return count;
+    }
+
+    private static boolean containsDuplicates(int[] a) {
+        for (int i = 1; i < a.length; i++)
+            if (a[i] == a[i-1]) return true;
+
+        return false;
+    }
+}
